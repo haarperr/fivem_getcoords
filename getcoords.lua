07753000027df98822eefa8c6aef5daf1c45903a
Harry <42916213+harrydiscord@users.noreply.github.com>
2019-03-23 14:18:41 +0000

    Add files via upload

---
+++ getcoords.lua
@@ -0,0 +1,8 @@
+RegisterCommand("getcoords", function()
+    x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
+		
+    roundx = tonumber(string.format("%.2f", x))
+    roundy = tonumber(string.format("%.2f", y))
+	roundz = tonumber(string.format("%.2f", z))
+	notify("~r~X~s~: "..roundx.. " ~r~Y~s~: "..roundy.." ~r~Z~s~: "..roundz)
+end)
